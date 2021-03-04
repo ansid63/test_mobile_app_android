52b9dbf44f04c205a30be49fcff424a33c666dc6
Andrey <sidorov.avk@gmail.com>
2021-03-04 20:25:41 +0400

    first test suite done

---
+++ TestData.py
@@ -0,0 +1,15 @@
+class Locators:
+    menu_button = 'com.alibaba.aliexpresshd:id/left_action'
+    login_menu_button = 'com.alibaba.aliexpresshd:id/profile_name_text'
+    log_button = 'com.alibaba.aliexpresshd:id/btn_sign_in'
+    email_input_field = 'com.alibaba.aliexpresshd:id/et_email'
+    password_input_field = 'com.alibaba.aliexpresshd:id/et_password'
+    confirm_button = 'com.alibaba.aliexpresshd:id/tv_signin_btn_label'
+    dialog_window_el = 'android:id/message'
+    order_button_el = 'com.alibaba.aliexpresshd:id/tv_all_orders'
+
+class Data:
+    WRONG_EMAIL = '77345@yahoo.com'
+    WRONG_PASSWORD = '12345'
+    CORRECT_EMAIL = 'email'
+    CORRECT_PASSWORD = 'password'
--- test_authorization.py
+++ test_authorization.py
@@ -2,41 +2,49 @@ import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
+from TestData import Locators, Data
 
-class TestAliAutorization():
+
+class TestAliAutorizationAndMenuSwipe():
+    
 
     def test_authorization_with_wrong_data(self, driver):
-        menu_element = WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, 'com.alibaba.aliexpresshd:id/left_action')))
+        menu_element = WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, Locators.menu_button)))
         menu_element.click()
-        authorization_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/profile_name_text')
+        authorization_button = driver.find_element(By.ID, Locators.login_menu_button)
         authorization_button.click()
-        login_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/btn_sign_in')
+        login_button = driver.find_element(By.ID, Locators.log_button)
         login_button.click()
-        email_field = driver.find_element_by_id('com.alibaba.aliexpresshd:id/et_email')
-        email_field.send_keys('77345@yahoo.com')
-        password_field = driver.find_element_by_id('com.alibaba.aliexpresshd:id/et_password')
-        password_field.send_keys('12345')
-        sign_in_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/tv_signin_btn_label')
+        email_field = driver.find_element(By.ID, Locators.email_input_field)
+        email_field.send_keys(Data.WRONG_EMAIL)
+        password_field = driver.find_element(By.ID, Locators.password_input_field)
+        password_field.send_keys(Data.WRONG_PASSWORD)
+        sign_in_button = driver.find_element(By.ID, Locators.confirm_button)
         sign_in_button.click()
-        dialog_window = driver.find_element_by_id('android:id/message')
+        dialog_window = driver.find_element(By.ID, Locators.dialog_window_el)
         dialog_window_text = dialog_window.text
         assert dialog_window_text == 'Password is incorrect. Please try again.'
 
 
     def test_authorization_correct_data(self, driver):
-        menu_element = WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, 'com.alibaba.aliexpresshd:id/left_action')))
+        menu_element = WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, Locators.menu_button)))
         menu_element.click()
-        authorization_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/profile_name_text')
+        authorization_button = driver.find_element(By.ID, Locators.login_menu_button)
         authorization_button.click()
-        login_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/btn_sign_in')
+        login_button = driver.find_element(By.ID, Locators.log_button)
         login_button.click()
-        email_field = driver.find_element_by_id('com.alibaba.aliexpresshd:id/et_email')
-        email_field.send_keys('email')
-        password_field = driver.find_element_by_id('com.alibaba.aliexpresshd:id/et_password')
-        password_field.send_keys('Password')
-        sign_in_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/tv_signin_btn_label')
+        email_field = driver.find_element(By.ID, Locators.email_input_field)
+        email_field.send_keys(Data.CORRECT_EMAIL)
+        password_field = driver.find_element(By.ID, Locators.password_input_field)
+        password_field.send_keys(Data.CORRECT_PASSWORD)
+        sign_in_button = driver.find_element(By.ID, Locators.confirm_button)
         sign_in_button.click()
-        order_button = driver.find_element_by_id('com.alibaba.aliexpresshd:id/tv_all_orders')
+        order_button = driver.find_element_by_id(Locators.order_button_el)
         order_button_enabled = order_button.is_enabled()
         order_button_text = order_button.text
         assert order_button_enabled is True and order_button_text == 'My Orders'
+
+    def test_menu_swipe(self, driver):
+        menu_element = WebDriverWait(driver, 3).until(expected_conditions.element_to_be_clickable((By.ID, Locators.menu_button)))
+        driver.swipe(0, 1080, 500, 1080, 100)
+        time.sleep(3)
