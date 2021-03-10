638144c3f26980a2f111262649e853885cbac7d6
Andrey <sidorov.avk@gmail.com>
2021-03-10 18:29:13 +0400

    desired caps update

--- conftest.py
+++ conftest.py
@@ -10,10 +10,11 @@ def driver(request):
         "platformName": "Android",
         "platformVersion": "9",
         "deviceName": "Samsung Galaxy S9",
+        "app": "storage:61f3316b-f0bf-4df0-9127-d2758b1e67f2",
         "app_package": "com.alibaba.aliexpresshd",
         "build": "build-252",
         "noReset": "false",
-        "testobject_api_key": "f873206f-f7a2-43d3-a593-99a61c2a3cbd"
+        "testobject_api_key": "f873206f-f7a2-43d3-a593-99a61c2a3cbd",
     }
     EXECUTOR = "https://andrey.sidorov:f873206f-f7a2-43d3-a593-99a61c2a3cbd@ondemand.eu-central-1.saucelabs.com:443/wd/hub"
     caps = copy.copy(ANDROID_BASE_CAPS)
